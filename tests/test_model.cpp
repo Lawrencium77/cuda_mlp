8fb3ef03f9a00301c2c8574b795c200f4cd5d124
Lawrencium77 <108270744+Lawrencium77@users.noreply.github.com>
2024-11-05 22:37:35 +0000

    WIP: Matrix and Model tests looking OK (#12)
    * WIP: Matrix and Model tests looking OK
    
    * Training working fine
    
    * Remove test_utils.h

--- tests/test_model.cpp
+++ tests/test_model.cpp
@@ -1,23 +1,19 @@
-#include "test_utils.h"
 #include "model.h"
+#include <iostream>
 
-void testSingleLayerForward(float* data, Matrix& input, int bsz, int feat_dim) {
+void testSingleLayerForward(Matrix& input, int bsz, int feat_dim) {
   SingleLayerPerceptron slp(feat_dim, feat_dim);
   slp.randomise(0);
   Matrix output = slp.forward(input);
-  
-  output.getData(data);
-  printMatrixData(data, bsz, feat_dim);
+  output.printData("SLP Output");
 }
 
-void testSingleLayerBackward(float* data, Matrix& input, int bsz, int feat_dim) {
+void testSingleLayerBackward(Matrix& input, int bsz, int feat_dim) {
   SingleLayerPerceptron slp(feat_dim, feat_dim);
   slp.randomise(0);
   Matrix output = slp.forward(input);
   Matrix grad = slp.backward(output);
-  
-  grad.getData(data);
-  printMatrixData(data, bsz, feat_dim);
+  grad.printData("SLP Gradient");
 }
 
 Matrix getLabels(int bsz) {
@@ -26,23 +22,21 @@ Matrix getLabels(int bsz) {
       labels_data[i] = 0;
   }
   Matrix labels(bsz, 1);
-  labels.setData(labels_data);
+  labels.setHostData(labels_data);
+  labels.toDevice();
   return labels;
 }
 
-void testMLPForward(float* data, Matrix& input, int bsz, int feat_dim, int num_layers, int num_classes) {
+void testMLPForward(Matrix& input, int bsz, int feat_dim, int num_layers, int num_classes) {
   MLP mlp(feat_dim, num_layers);
   mlp.randomise(0);
   Matrix output = mlp.forward(input);
-  
-  output.getData(data);
-  printMatrixData(data, bsz, num_classes);
+  output.printData("MLP Output");
 
   Matrix labels = getLabels(bsz);
 
   Matrix losses = get_ce_loss(output, labels);
-  losses.getData(data);
-  printMatrixData(data, 1, bsz);
+  losses.printData("MLP Losses");
 }
 
 void testMLPBackward(Matrix& input, int feat_dim, int num_layers, int num_classes, int bsz) {
@@ -57,16 +51,13 @@ void testMLPBackward(Matrix& input, int feat_dim, int num_layers, int num_classe
   // Examine first layer gradient
   float* data = new float[feat_dim * feat_dim];
   Matrix grads = mlp.layers[0].grads;
-  grads.getData(data);
-  printMatrixData(data, feat_dim, feat_dim, "for first layer gradients");
+  grads.printData("First layer gradients");
 
   // Update weights
   float lr = 1.0;
-  mlp.layers[0].weights.getData(data);
-  printMatrixData(data, feat_dim, feat_dim, "before weight update");
+  mlp.layers[0].weights.printData("Before weight update");
   mlp.update_weights(lr);
-  mlp.layers[0].weights.getData(data);
-  printMatrixData(data, feat_dim, feat_dim, "after weight update");
+  mlp.layers[0].weights.printData("After weight update");
 }
 
 void setHostDataToConst(float* data, int numel, float value) {
@@ -78,29 +69,25 @@ void setHostDataToConst(float* data, int numel, float value) {
 void runTests() {
   int feat_dim = 8;
   int bsz = 4;
+  int num_layers = 4;
   int num_classes = 10;
   int input_numel = feat_dim * bsz;
   int output_numel = num_classes * bsz;
   
   float* input_data = new float[input_numel];
   setHostDataToConst(input_data, input_numel, 1.0f);
+  Matrix input = Matrix(bsz, feat_dim);
+  input.setHostData(input_data);
+  input.toDevice();
 
   std::cout << "Testing Single Layer" << std::endl;
-  Matrix input = Matrix(bsz, feat_dim);
-  input.setData(input_data);
-  testSingleLayerForward(input_data, input, bsz, feat_dim);
-  testSingleLayerBackward(input_data, input, bsz, feat_dim);
+  testSingleLayerForward(input, bsz, feat_dim);
+  testSingleLayerBackward(input, bsz, feat_dim);
 
   std::cout << "Testing MLP" << std::endl;
-  int num_layers = 4;
-  float* output_data = new float[output_numel];
-  setHostDataToConst(input_data, input_numel, 1.0f);
-  testMLPForward(output_data, input, bsz, feat_dim, num_layers, num_classes);
-
+  setHostDataToConst(input.host_data, input_numel, 1.0f);
+  testMLPForward(input, bsz, feat_dim, num_layers, num_classes);
   testMLPBackward(input, feat_dim, num_layers, num_classes, bsz);
-
-  delete [] input_data;
-  delete [] output_data;
 }
 
 int main() {
